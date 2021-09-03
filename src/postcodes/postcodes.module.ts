fc6fe9df3d3aee546875e8edc0a4b4194b701607
riley909 <increaseblue666@gmail.com>
2021-09-03 11:33:37 +0900

    add PostcodesService

--- src/postcodes/postcodes.module.ts
+++ src/postcodes/postcodes.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PostcodesController } from './postcodes.controller';
+import { PostcodesService } from './postcodes.service';
 
 @Module({
   controllers: [PostcodesController],
+  providers: [PostcodesService],
 })
 export class PostcodesModule {}
