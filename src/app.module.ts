3f3e94bf44270600c6b4e9eb91453424f8b7fc07
riley909 <increaseblue666@gmail.com>
2021-09-03 14:47:56 +0900

    remove locations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LocationsModule } from './locations/locations.module';
 
 @Module({
-  imports: [LocationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
