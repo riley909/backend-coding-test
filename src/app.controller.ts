d7fa6d1f0cdf3d9a6181fcc7a1b38e80b698507f
riley909 <increaseblue666@gmail.com>
2021-09-04 20:53:07 +0900

    add getSurroundingList

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { GetSurroundingListDto } from './dto/get-surrounding-list.dto';
 import { Location } from './models/locations.model';
 import { Store } from './models/stores.model';
 
@@ -20,5 +21,10 @@ export class AppController {
   @Get('location/:postcode')
   getLocation(@Param('postcode') postcode: string): Promise<Location> {
     return this.appService.getLocation(postcode);
+  }
+
+  @Post('/surrounding')
+  getSurroundingList(@Body() surroundingListDto: GetSurroundingListDto) {
+    return this.appService.getSurroundingList(surroundingListDto);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import * as data from '../stores.json';
 import { Store } from './models/stores.model';
 import axios, { AxiosResponse } from 'axios';
 import { Location } from './models/locations.model';
+import { GetSurroundingListDto } from './dto/get-surrounding-list.dto';
 
 @Injectable()
 export class AppService {
@@ -20,5 +21,60 @@ export class AppService {
     );
     const { longitude, latitude } = res.data.result;
     return { longitude, latitude };
+  }
+
+  async getSurroundingList(surroundingListDto: GetSurroundingListDto) {
+    const { postcode, radius } = surroundingListDto;
+
+    try {
+      // 목록에서 검색 대상인 지역을 제외하기
+      const targetList = data.filter((el) => {
+        return (
+          el.postcode.replace(/\s/g, '').toLowerCase() !==
+          postcode.toLowerCase()
+        );
+      });
+
+      // 대상의 postcode로 경도와 위도 가져오기
+      const targetLocation = await this.getLocation(postcode);
+      const { longitude, latitude } = targetLocation;
+
+      // 받아온 경도, 위도 + 파라미터 radius로 대상의 범위에 위치한 지역 찾기
+      const res = await axios.post(`https://api.postcodes.io/postcodes`, {
+        geolocations: [
+          {
+            longitude,
+            latitude,
+            radius,
+          },
+        ],
+      });
+
+      // 북->남 으로 정렬하기 위해 경도, 위도 가져오기
+      const arrRes = res.data.result[0].result.map((el) => {
+        return {
+          postcode: el.postcode,
+          longitude: el.longitude,
+          latitude: el.latitude,
+        };
+      });
+
+      arrRes.sort((a, b) => {
+        if (a.latitude > b.latitude) -1;
+        else 1;
+      });
+
+      const surroundingList = targetList.filter((store: Store) => {
+        return arrRes.find((el) => {
+          if (store.postcode === el.postcode) {
+            return el;
+          }
+        });
+      });
+
+      return surroundingList;
+    } catch (e) {
+      throw new NotFoundException(`찾을 수 없는 우편번호 입니다`);
+    }
   }
 }
